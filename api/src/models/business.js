9981bc162a56ef217fc5fc4180801c4592fd1696
LautaroZalazar <lautyzalazar10@gmail.com>
2022-09-30 00:05:35 -0300

    model business export

--- api/src/models/business.js
+++ api/src/models/business.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const business = mongoose.model(
     'business',
@@ -27,3 +27,5 @@ const business = mongoose.model(
         ]
     })
 )
+
+export default business
